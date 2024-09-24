10908e4196a9ed3bf39d0cdc00acecfafb6cc4ae
Mamaev <rostislavmamaev@yandex.ru>
2024-09-24 04:42:28 +0300

    Update lab1.cpp

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,1 +1,39 @@
-﻿
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	string path = "main.txt";
+
+	ifstream fin;
+	
+	fin.open(path);
+
+	if (!fin.is_open())
+	{
+		cout << "Ошибка открытия файла!" << endl;
+	}
+	else
+	{
+		cout << "Файл открыт успешно!" << endl;
+		string str;
+
+		while (!fin.eof())
+		{
+			str = "";
+			getline(fin, str);
+			cout << "Hello, " << str << "!" << endl;
+		}
+	}
+
+	fin.close();
+
+	return 0;
+}
